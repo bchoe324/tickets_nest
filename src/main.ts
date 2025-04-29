a8a658c2bc2b8af65a0a8cb29b0272434c0f2323
boramchoi <bchoe324@gmail.com>
2025-04-29 17:53:33 +0900

    주소 수정..

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://tickets-ten-pi.vercel.app/'],
+    origin: ['https://tickets-ten-pi.vercel.app'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
