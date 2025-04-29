a76016764a8a4fc5b639001c90834ed6371b4130
boramchoi <bchoe324@gmail.com>
2025-04-30 01:12:29 +0900

    vercel 제거, 테스트

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
+console.log(process.env);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['https://tickets-ten-pi.vercel.app'],
     credentials: true,
+    optionsSuccessStatus: 200,
   });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(cookieParser());
