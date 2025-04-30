94806bb6e7fc6dadbea4a26ea64555a727943225
boramchoi <bchoe324@gmail.com>
2025-04-30 11:48:53 +0900

    fix: refactor CORS configuration for improved clarity and structure

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,27 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   console.log('🔥 Starting Nest Factory...');
   const app = await NestFactory.create(AppModule);
   console.log('✅ NestFactory created');
 
-  app.enableCors({
-    origin: (origin, callback) => {
+  const corsOptions: CorsOptions = {
+    origin: (
+      origin: string | undefined,
+      callback: (err: Error | null, allow?: boolean) => void,
+    ) => {
       console.log('🌐 Incoming request origin:', origin);
       callback(null, true);
     },
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  });
+  };
+
+  app.enableCors(corsOptions);
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.use(cookieParser());
