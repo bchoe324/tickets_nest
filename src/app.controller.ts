5a42141e9aa303fb7cd4af6f415ca62eeca2f06c
boramchoi <bchoe324@gmail.com>
2025-04-30 23:40:40 +0900

    fix: update getHello method to return server status message

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getRoot() {
+    return '✅ NestJS server is running!';
   }
 }
